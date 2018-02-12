47b136f1e0e2174142929003d938cb62bb2ac503
DESKTOP-I79COB3\pegasus-s <fenrir7320s@gmail.com>
2018-02-12 19:45:42 +0900

    chenge to "string" char型をstring型に変更 string型をうまく使うために"sstream"をインクルード

--- Main.h
+++ Main.h
@@ -4,11 +4,14 @@
 #define _MAIN_H_INCLUDED_ //ヘッダを読み込んだら定義する⇒次にヘッダが読み込まれてもifndefに入らず無視できる
 #include "DxLib.h"
 #include "windows.h"
+#include "sstream"
+
+using namespace std;
 
 //定数定義
-#define WINDOW_WIDTH  1024
+#define WINDOW_WIDTH  960
 #define WINDOW_HEIGHT 768
-#define MAP_WIDTH 11
+#define MAP_WIDTH 10
 #define MAP_HEIGHT 12
 #define CHIP_SIZE 64
 
@@ -23,13 +26,17 @@ int flag : 選択中かどうかのフラグ(1:選択中 0:未選択)
 *****/
 typedef struct {
 	int x, y;       // 座標格納用変数
-	char name[128]; // 項目名格納用変数
+	string name; // 項目名格納用変数
 	int flag;
 } MenuElement;
 
 MenuElement MainMenu[2] = {
 	{ 500, 500, "ゲームスタート", 1 }, // タグの中身の順番で格納される。xに80が、yに100が、nameに"ゲームスタート"が
 	{ 500, 550, "ゲーム終了", 0 },
+};
+
+MenuElement debug[1] = {
+	{ 768, 550, "キャラ座標(", 0 },
 };
 
 #endif
